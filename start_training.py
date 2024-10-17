05a6a2ad8a57439d7bcb5df829f346213340b686
Mayank Pratap Singh <mayankpratapsingh022@gmail.com>
2024-10-17 22:25:38 +0530

    training pipeline strcture updated

--- start_training.py
+++ start_training.py
@@ -10,7 +10,8 @@ from networksecurity.pipeline.training_pipeline import TrainingPipeline
 
 def start_training():
     try:
-        pass
+        model_training =TrainingPipeline()
+        model_training.run_pipeline()
     except Exception as e:
         raise NetworkSecurityException(e,sys)
     
@@ -18,4 +19,4 @@ def start_training():
 if __name__=='__main__':
     start_training()
 
-    
+
