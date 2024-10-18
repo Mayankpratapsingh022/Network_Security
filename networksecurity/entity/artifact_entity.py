458cdaaaf3c91bbdda0e264424b9f48c557bd769
Mayank Pratap Singh <mayankpratapsingh022@gmail.com>
2024-10-18 23:05:37 +0530

    config and artifact is ready

--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -6,7 +6,10 @@ from networksecurity.constant import training_pipeline
 
 @dataclass 
 class DataIngestionArtifact:
-    pass 
+    trained_file_path: str 
+    test_file_path: str
+
+
 
 
 @dataclass
