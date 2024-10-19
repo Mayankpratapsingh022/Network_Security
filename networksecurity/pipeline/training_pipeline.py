de26c93361eed5b4028e66c8d6a6e1856968720f
Mayank Pratap Singh <mayankpratapsingh022@gmail.com>
2024-10-19 21:55:14 +0530

    data validation started

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -62,9 +62,12 @@ class TrainingPipeline:
 
 
 
-    def start_data_validation(self):
+    def start_data_validation(self,data_ingestion_artifact:DataIngestionArtifact):
         try:
-            pass
+            data_validation_config = DataValidationConfig(training_pipeline_config= self.training_pipeline_config)
+            data_validation = DataValidation(data_ingestion_artifact = data_ingestion_artifact ,data_validation_config=data_validation_config,)
+            data_validation_artifact = data_validation.initiate_date_validation()
+            return data_validation_artifact
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
@@ -97,6 +100,7 @@ class TrainingPipeline:
         try:
             data_ingestion_artifact= self.start_data_ingestion()
             print(data_ingestion_artifact)
+            data_validation = self.start_data_validation()
 
         except Exception as e:
             raise NetworkSecurityException(e,sys)
